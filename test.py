cb6e3068dfc740f72ec77aa5bc390e2259f59e69
bjk2023 <barbarakotlannico@gmail.com>
2025-09-26 19:31:34 -0400

    Mock ML Stock testing

---
+++ test.py
@@ -0,0 +1,49 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.ar_model import AutoReg
+
+def create_dataset(period):
+    data = yf.download("AAPL", period=period, auto_adjust=False)
+    df = pd.DataFrame(data)
+    df = df.drop(columns=["Adj Close", "High","Low", "Open", "Volume"])
+    return df
+
+def normalize_data(df):
+    num_cols = ['Close']
+    
+    row_sum = df[num_cols].sum(axis=1)
+    
+    df[num_cols] = df[num_cols].div(row_sum, axis=0)
+    
+    return df
+
+def estimate_new(df, numdays=1):
+    predictions = []
+
+    for _ in range(numdays):
+        values = df['Close'].values
+        lags = max(1, len(values)//4)  # automatically choose lag
+        model = AutoReg(values, lags=lags)
+        model_fit = model.fit()
+
+        # Predict next day
+        next_day_pred = model_fit.predict(start=len(values), end=len(values))
+        next_date = df.index[-1] + pd.Timedelta(days=1)
+
+        # Add prediction to DataFrame
+        df.loc[next_date] = next_day_pred[0]
+
+        # Keep track of predictions
+        predictions.append(next_day_pred[0])
+
+    return next_day_pred[0]
+
+        
+
+if __name__ == "__main__":
+    dataset = create_dataset('10y')
+    numdays = 31
+    next_day_estimate = estimate_new(dataset, numdays)
+    print(dataset.tail(10+numdays))
+    print(next_day_estimate)
