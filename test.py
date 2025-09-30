ed987e0661e44872e7ec68c886349e8eba8a4efc
bjk2023 <barbarakotlannico@gmail.com>
2025-09-30 15:57:54 -0400

    Implement prediction for today

--- test.py
+++ test.py
@@ -18,6 +18,19 @@ def normalize_data(df):
     
     return df
 
+def test_today(df):
+    today = df["Close"].iloc[-1].item()
+    df = df.iloc[:-1]
+
+    values = df['Close'].values
+    lags = max(1, len(values)//4)
+    model = AutoReg(values, lags=lags)
+    model_fit = model.fit()
+
+    next_day_pred = model_fit.predict(start=len(values), end=len(values))[0]
+
+    return today, next_day_pred
+
 def estimate_new(df, numdays=1):
     predictions = []
 
@@ -28,22 +41,25 @@ def estimate_new(df, numdays=1):
         model_fit = model.fit()
 
         # Predict next day
-        next_day_pred = model_fit.predict(start=len(values), end=len(values))
+        next_day_pred = model_fit.predict(start=len(values), end=len(values))[0]
         next_date = df.index[-1] + pd.Timedelta(days=1)
 
         # Add prediction to DataFrame
-        df.loc[next_date] = next_day_pred[0]
+        df.loc[next_date] = next_day_pred
 
         # Keep track of predictions
-        predictions.append(next_day_pred[0])
+        predictions.append(next_day_pred)
 
-    return next_day_pred[0]
+    return next_day_pred
 
         
 
 if __name__ == "__main__":
     dataset = create_dataset('10y')
     numdays = 31
-    next_day_estimate = estimate_new(dataset, numdays)
-    print(dataset.tail(10+numdays))
-    print(next_day_estimate)
+    #next_day_estimate = estimate_new(dataset, numdays)
+    #print(dataset.tail(10+numdays))
+    #print(next_day_estimate)
+    test = test_today(dataset)
+    print("Expected: {}, Actual: {}".format(test[0], test[1]))
+
