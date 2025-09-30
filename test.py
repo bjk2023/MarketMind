21e535bf2c2d4b80529d3560805fa0a31adf09e7
bjk2023 <barbarakotlannico@gmail.com>
2025-09-30 17:18:39 -0400

    updating predictor

--- test.py
+++ test.py
@@ -1,65 +1,64 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
 from statsmodels.tsa.ar_model import AutoReg
 
-def create_dataset(period):
-    data = yf.download("AAPL", period=period, auto_adjust=False)
+
+#Creating a dataset based on ticker and period
+def create_dataset(ticker, period):
+    data = yf.download(ticker, period=period, auto_adjust=False)
     df = pd.DataFrame(data)
     df = df.drop(columns=["Adj Close", "High","Low", "Open", "Volume"])
     return df
 
-def normalize_data(df):
-    num_cols = ['Close']
-    
-    row_sum = df[num_cols].sum(axis=1)
-    
-    df[num_cols] = df[num_cols].div(row_sum, axis=0)
-    
-    return df
-
+#Testing model on todays value - removing last row and predicting it to compare
 def test_today(df):
-    today = df["Close"].iloc[-1].item()
+    date = df.index[-1]
+    today_value = df["Close"].iloc[-1].item()
     df = df.iloc[:-1]
 
-    values = df['Close'].values
-    lags = max(1, len(values)//4)
-    model = AutoReg(values, lags=lags)
-    model_fit = model.fit()
+    new_df = estimate_new(df, numdays=1)
 
-    next_day_pred = model_fit.predict(start=len(values), end=len(values))[0]
+    return date, today_value, new_df["Close"].iloc[-1].item()
 
-    return today, next_day_pred
-
+#Estimating next day(s) value based on previous values
 def estimate_new(df, numdays=1):
     predictions = []
+    df_new = df.copy()
 
     for _ in range(numdays):
-        values = df['Close'].values
-        lags = max(1, len(values)//4)  # automatically choose lag
+        values = df_new['Close'].values
+        lags = 1
         model = AutoReg(values, lags=lags)
         model_fit = model.fit()
 
-        # Predict next day
         next_day_pred = model_fit.predict(start=len(values), end=len(values))[0]
-        next_date = df.index[-1] + pd.Timedelta(days=1)
+        next_date = df_new.index[-1] + pd.Timedelta(days=1)
 
-        # Add prediction to DataFrame
-        df.loc[next_date] = next_day_pred
+        df_new.loc[next_date] = next_day_pred
 
-        # Keep track of predictions
         predictions.append(next_day_pred)
 
-    return next_day_pred
-
-        
+    return df_new
 
 if __name__ == "__main__":
-    dataset = create_dataset('10y')
-    numdays = 31
-    #next_day_estimate = estimate_new(dataset, numdays)
-    #print(dataset.tail(10+numdays))
-    #print(next_day_estimate)
-    test = test_today(dataset)
-    print("Expected: {}, Actual: {}".format(test[0], test[1]))
+    #Create Dataset
+    dataset = create_dataset('AAPL', '100y')
+    print(dataset.tail(5))
+    print(len(dataset))
 
+    #Test model on todays value based on past 5 days
+    data_past_five = dataset.iloc[len(dataset)-5:len(dataset)]
+    test = test_today(data_past_five)
+    print("Date: {}, Expected: {}, Actual: {}".format(test[0], test[1], test[2]))
+
+    #Test model on todays value based on all previous values
+    test = test_today(dataset)
+    print("Date: {}, Expected: {}, Actual: {}".format(test[0], test[1], test[2]))
+
+    #Estimate next 5 days based on past 5 days
+    new_df = estimate_new(data_past_five, numdays=5)
+    print(new_df.iloc[len(new_df)-5:])
+
+    #Estimate next 5 days based on all previous values
+    new_df = estimate_new(dataset, numdays=5)
+    print(new_df.iloc[len(new_df)-5:])
